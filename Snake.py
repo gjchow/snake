9166136146fba3a55725134a48034077b461a4ca
Gabriel Chow <36006921+gjchow@users.noreply.github.com>
2021-02-14 21:32:18 -0500

    Added two-player

--- Snake.py
+++ Snake.py
@@ -32,9 +32,10 @@ class Food(object):
 
 
 class Snake(object):
-    def __init__(self):
-        self.body = [(0, 0)]
-        self.head = 0, 0
+    def __init__(self, x, y, colour):
+        self.body = [(x, y)]
+        self.head = x, y
+        self.colour = colour
 
     def __len__(self):
         return len(self.body)
@@ -76,9 +77,10 @@ class Snake(object):
     def draw(self):
         for coord in self.body:
             if self.head == coord:
-                colour = (0, 200, 0)
+                colour = (int(self.colour[0]*0.8), int(self.colour[1]*0.8),
+                          int(self.colour[2]*0.8))
             else:
-                colour = (0, 255, 0)
+                colour = self.colour
             rect = pygame.Rect(coord[0]*grid, coord[1]*grid, grid, grid)
             pygame.draw.rect(screen, colour, rect)
 
@@ -94,7 +96,7 @@ class Game:
 
     def run(self):
         direction = 1
-        s = Snake()
+        s = Snake(0, 0, (0, 255, 0))
         f = Food()
         check = False
         save = 0
@@ -138,12 +140,103 @@ class Game:
                 f.draw()
                 s.draw()
                 pygame.display.flip()
+                pygame.time.delay(200-((len(s)-1)*10))
+                clock.tick(60)
+
+
+class GameTwo(Game):
+    def run(self):
+        d1 = 1
+        s1 = Snake(0, 0, (0, 255, 0))
+        d2 = 3
+        s2 = Snake(rows - 1, rows - 1, (0, 0, 255))
+        f = Food()
+        check = False
+        save1 = 0
+        save2 = 0
+        while not self.done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    if d1 != 0:
+                        save1 = d1
+                        save2 = d2
+                        d1 = 0
+                        d2 = 0
+                    else:
+                        d1 = save1
+                        d2 = save2
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                    if d1 != 3 and d1 != 0:
+                        d1 = 1
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                    if d1 != 4 and d1 != 0:
+                        d1 = 2
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                    if d1 != 1 and d1 != 0:
+                        d1 = 3
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                    if d1 != 2 and d1 != 0:
+                        d1 = 4
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                    if d2 != 3 and d2 != 0:
+                        d2 = 1
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                    if d2 != 4 and d2 != 0:
+                        d2 = 2
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                    if d2 != 1 and d2 != 0:
+                        d2 = 3
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                    if d2 != 2 and d2 != 0:
+                        d2 = 4
+            while not check:
+                if f.pos() in s1.body or f.pos() in s2.body:
+                    f.remake()
+                else:
+                    check = True
+            if s1.eat(f) or s2.eat(f):
+                f.remake()
+                check = False
+            if s1.head in s2.body and s2.head in s1.body:
+                print("Tie")
+                self.game_over()
+                self.done = True
+            elif s1.head in s2.body:
+                print("Player1 Loses")
+                self.game_over()
+                self.done = True
+            elif s2.head in s1.body:
+                print("Player2 Loses")
+                self.game_over()
+                self.done = True
+            if not self.done:
+                screen.fill((0, 0, 0))
+            m1 = s1.move(d1, f)
+            m2 = s2.move(d2, f)
+            if not m1 and not m2:
+                print("Tie")
+                self.game_over()
+                self.done = True
+            elif not m1:
+                print("Player1 Loses")
+                self.game_over()
+                self.done = True
+            elif not m2:
+                print("Player2 Loses")
+                self.game_over()
+                self.done = True
+            if not self.done:
+                f.draw()
+                s1.draw()
+                s2.draw()
+                pygame.display.flip()
                 pygame.time.delay(200)
-                clock.tick(30)
+                clock.tick(60)
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = GameTwo()
     game.run()
-
-
